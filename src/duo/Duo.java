494c9bee6a0280eac894f22c5347e47eccd2c982
kemajou85 <kemajou85@yahoo.fr>
2015-10-16 16:19:26 +0200

    function v2

--- src/duo/Duo.java
+++ src/duo/Duo.java
@@ -11,15 +11,27 @@ package duo;
  */
 public class Duo {
  public int i;
+ public static int fin=0;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("resultat: "+factorielle(3));
+        
     }
     
-    public int factorielle(){
-        return i;
+    public static int factorielle(int a){
+        if((a-1)>=0){
+            
+        System.out.println("factorielle de "+a);
+            fin=fin+(a+(a-1));
+            factorielle(a-1);
+        }
+        return fin;
     }
-  
+    
+   public int factorielle2(int a){
+        return a+a;
+    }
 }
